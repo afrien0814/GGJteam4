8da0aee5afd10584446375f461829f2082642c17
afrien0814 <49971202+afrien0814@users.noreply.github.com>
2021-01-29 21:37:14 +0800

    Initial commit

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField]
+    public string directrion;
+    [SerializeField]
+    private KeyCode upKey, downKey, leftKey, rightKey;
+    private float moveSpeed = 3;
+    private Vector2 dir = Vector2.zero;
+    private Rigidbody2D rigidBody2D;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dir = Vector2.zero;
+        GetKeyInput();
+        rigidBody2D.velocity=dir;
+    }
+
+    private void GetKeyInput()
+    {
+        if (Input.GetKey(upKey)) dir.y = moveSpeed;
+        if (Input.GetKey(downKey)) dir.y = -moveSpeed;
+        if (Input.GetKey(rightKey)) dir.x = moveSpeed;
+        if (Input.GetKey(leftKey)) dir.x = -moveSpeed;
+        
+    }
+
+}
