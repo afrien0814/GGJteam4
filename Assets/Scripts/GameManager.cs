257a9d579ef410b3a2b4db3a7ec096498433e22e
A_HAN <superman1230000@gmail.com>
2021-01-30 03:11:21 +0800

    edit game manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager gameManager;
     public float timer = 180;
+    public GameObject[] players;
+    public int[] target_list;
+    class container_player{
+        public int pid;
+        public container_player(int i){
+            pid = i;
+        }
+    }
 
     // Start is called before the first frame update
+    void Awake(){
+        if(gameManager == null){
+            gameManager = this;
+        }else{
+            Destroy(gameManager.gameObject);
+        }
+        initiation();
+    }
+    
     void Start()
     {
         
@@ -17,4 +36,38 @@ public class GameManager : MonoBehaviour
     {
         
     }
+    ///////////////////////////////////////////////////////////
+    // DO NOT LOOK INTO IT YOU PROBABLY WILL DIE FOR THIS
+    ///////////////////////////////////////////////////////////
+
+    void initiation(){
+        List<container_player> pl = new List<container_player>();
+        target_list = new int[4];
+        bool ok = false;
+        while(!ok){
+            for(int i=0;i<players.Length;i++){
+                pl.Add(new container_player(players[i].GetComponent<Move>().playerId));
+                print(pl[i].pid);
+            }
+            for(int i=0;i<players.Length;i++){
+                int rand_target = Random.Range(0,pl.Count);
+                print("player "+players[i].GetComponent<Move>().playerId+"'s target is "+pl[rand_target].pid);
+                target_list[i] = pl[rand_target].pid;
+                pl.Remove(pl[rand_target]);
+            }
+            bool check = false;
+            for(int i=0;i<players.Length;i++){
+                if(players[i].GetComponent<Move>().playerId == target_list[i]){
+                    check=true;
+                }
+            }
+            if(check == false){
+                ok = true;
+            }
+        }
+    }
+    public void win(int winner){
+        
+    }
+
 }
--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -2,71 +2,123 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Tilemaps;
 using System.IO;
 public class MapGenerator : MonoBehaviour
 {
-    List<string[]> map = new List<string[]>();
-    [Tooltip("每一個方格的大小(unity內部單位)")]
-    public Vector2 unit_scale;
-    public GameObject[] prefab_list;public GameObject outer_map_obj;
-    public string[] prefab_name;
-    public int width = 0;
-    public int length = 0;
+    class tile_info{
+
+    }
+    //List<string[]> map = new List<string[]>();
+    //[Tooltip("每一個方格的大小(unity內部單位)")]
+    //public Vector2 unit_scale;
+    public List<Vector2> empty_grid = new List<Vector2>();
+    public GameObject[] prefab_list;//public GameObject outer_map_obj;
+    public int[] prefab_amount;
+    //public string[] prefab_name;
+    //public int width = 0;
+    //public int length = 0;
+    void Awake(){
+        //read_map();
+        //generate_map();
+        //generate_random();
+    }
     void Start()
     {
-        read_map();
-        generate_map();
+        
     }
 
     void Update()
     {
         
     }
-
+    public GameObject red,green;
     void read_map(){
-        StreamReader sr = new StreamReader(Application.streamingAssetsPath + "/map_test.csv");
-        try {
-            string line;bool first = true;
-            while ((line = sr.ReadLine()) != null)
-            {
-                string[] row = line.Split(',');
-                if(first){
-                    width = row.Length;
-                    first = false;
+        // StreamReader sr = new StreamReader(Application.streamingAssetsPath + "/map_test.csv");
+        // try {
+        //     string line;bool first = true;
+        //     while ((line = sr.ReadLine()) != null)
+        //     {
+        //         string[] row = line.Split(',');
+        //         if(first){
+        //             width = row.Length;
+        //             first = false;
+        //         }
+        //         if(row.Length == width){
+        //             map.Add(row);
+        //         }
+        //     }
+        //     length = map.Count;
+        // }
+        // catch (UnityException e)
+        // {
+        //     // Let the user know what went wrong.
+        //     print("The file could not be read:");
+        //     print(e.Message);
+        // }
+        Tilemap tilemap = FindObjectOfType<Tilemap>();
+
+        BoundsInt bounds = tilemap.cellBounds;
+        TileBase[] allTiles = tilemap.GetTilesBlock(bounds);bool fisrt = true;
+        Grid gr = tilemap.gameObject.GetComponentInParent<Grid>();
+        
+        for (int x = 0; x < bounds.size.x; x++) {
+            for (int y = 0; y < bounds.size.y; y++) {
+                TileBase tile = allTiles[x + y * bounds.size.x];
+                if(fisrt){
+                    //print("x"+tilemap.CellToWorld(new Vector3Int(x,y,0)).x+"y"+tilemap.CellToWorld(new Vector3Int(x,y,0)).y+"x*y"+bounds.size.x*bounds.size.y);
+                    print("!!!x"+tilemap.LocalToCell(new Vector3(0,0,0)).x+"y"+tilemap.LocalToCell(new Vector3(0,0,0)).y);
+                    fisrt = !fisrt;
+                    print(tile);
                 }
-                if(row.Length == width){
-                    map.Add(row);
+                if (tile != null) {
+                    Debug.Log("x:" + x + " y:" + y + " tile:" + tile.name);
+                    //print("x"+tilemap.CellToLocal(new Vector3Int(x,y,0)).x+"y"+tilemap.CellToLocal(new Vector3Int(x,y,0)).y+"x*y"+bounds.size.x*bounds.size.y);
+                    //print("x"+tilemap.CellToLocal(+"y"+tilemap.CellToWorld(new Vector3Int(x,y,0)).y+"x*y"+bounds.size.x*bounds.size.y);
+                    GameObject gm = Instantiate(red);
+                    gm.transform.position = new Vector3(x*gr.cellSize.x,y*gr.cellSize.y,0);
+                } else {
+                    Debug.Log("x:" + x + " y:" + y + " tile: (null)");
+                    //print("x"+tilemap.CellToLocal(new Vector3Int(x,y,0)).x+"y"+tilemap.CellToLocal(new Vector3Int(x,y,0)).y+"x*y"+bounds.size.x*bounds.size.y);
+                    empty_grid.Add(new Vector2(x,y));
                 }
             }
-            length = map.Count;
         }
-        catch (UnityException e)
-        {
-            // Let the user know what went wrong.
-            print("The file could not be read:");
-            print(e.Message);
+        for (int x = 0; x < bounds.size.x; x++) {
+            for (int y = 0; y < bounds.size.y; y++) {
+                TileBase tile = allTiles[x + y * bounds.size.x];
+                
+            }
         }
     }
     void generate_map(){
         // i=第幾行,j=某行第幾個元素
-        for(int i=0;i < map.Count;i++){
-            for(int j=0;j < map[i].Length;j++){
-                for(int prefab_index=0;prefab_index<prefab_list.Length;prefab_index++){
-                    if((map[i])[j] == prefab_name[prefab_index]){
-                        if((map[i])[j] != "0"){
-                            GameObject new_pref = Instantiate(prefab_list[prefab_index]);
-                            new_pref.transform.position = new Vector3(0,0,0)+new Vector3(i*unit_scale.x,j*unit_scale.y,0);
-                        }
-                    }
+        // for(int i=0;i < map.Count;i++){
+        //     for(int j=0;j < map[i].Length;j++){
+        //         for(int prefab_index=0;prefab_index<prefab_list.Length;prefab_index++){
+        //             if((map[i])[j] == prefab_name[prefab_index]){
+        //                 if((map[i])[j] != "0"){
+        //                     GameObject new_pref = Instantiate(prefab_list[prefab_index]);
+        //                     new_pref.transform.position = new Vector3(0,0,0)+new Vector3(i*unit_scale.x,j*unit_scale.y,0);
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
+
+    }
+    void generate_random(){
+        int rand;
+        //i=哪個prefab,j=重複生成次數
+        for(int i=0;i < prefab_list.Length;i++){
+            for(int j=0;j < prefab_amount[i];j++){
+                if(empty_grid.Count!=0){
+                    rand = Random.Range(0,empty_grid.Count);
+                    GameObject new_pref = Instantiate(prefab_list[i]);
+                    new_pref.transform.position = new Vector3(empty_grid[rand].x,empty_grid[rand].y,0);
                 }
             }
         }
-        generate_outer_wall();
-
-    }
-    void generate_outer_wall(){
-        for(int i=0;i<0;i++){
-
-        }
     }
 }
+
