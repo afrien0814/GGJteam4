04ca9f37be1a080d1ce6bfe15e58d5ebd5fcbdb7
afrien0814 <49971202+afrien0814@users.noreply.github.com>
2021-01-30 03:10:14 +0800

    dash

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -6,12 +6,17 @@ public class Move : MonoBehaviour
 {
     public int playerId;
     [SerializeField]
-    private KeyCode upKey, downKey, leftKey, rightKey;
-    private float moveSpeed = 5;
-    private Vector2 dir = Vector2.zero;
+    private KeyCode upKey, downKey, leftKey, rightKey, dashKey;
+    private float moveSpeed = 5f;
+    private Vector2 dir = Vector2.zero, lastDir = Vector2.zero;
     private Rigidbody2D rigidBody2D;
     private Collider2D myCollider;
     private List<KeyCode> keys;
+
+    [SerializeField]
+    private float dashCD = 1f, dashForce = 25f;
+    private bool dashing, dashInCD;
+    public Vector2 facing;
     
     // Start is called before the first frame update
     void Start()
@@ -23,6 +28,7 @@ public class Move : MonoBehaviour
         keys.AddRange(keys);
         keys.RemoveRange(4 + playerId - 1, 4 - playerId + 1);
         keys.RemoveRange(0, playerId - 1);
+        //for (int i = 0; i < keys.Count; i++) Debug.Log(keys[i].ToString());
         transform.GetChild(0).rotation = Quaternion.Euler(0, 0, 90 * (playerId - 1));
 
     }
@@ -30,23 +36,42 @@ public class Move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        dir = Vector2.zero;
         GetKeyInput();
         rigidBody2D.velocity=dir;
     }
 
     private void GetKeyInput()
     {
+        if (dashing) return;
+        if (dir != Vector2.zero) facing = dir.normalized;
+        dir = Vector2.zero;
         if (Input.GetKey(keys[0])) dir.y = moveSpeed;
         if (Input.GetKey(keys[1])) dir.x = moveSpeed;
         if (Input.GetKey(keys[2])) dir.y = -moveSpeed;
         if (Input.GetKey(keys[3])) dir.x = -moveSpeed;
-        
+        if (!dashInCD && Input.GetKeyDown(dashKey)) StartCoroutine(Dash());
+    }
+
+    private IEnumerator Dash()
+    {
+        dashing = true;
+        dir = facing * dashForce;
+        float acc = (dashForce - moveSpeed) / 3;
+        for(int timer = 0; timer < 3; timer++)
+        {
+            dir -= acc * facing;
+            yield return new WaitForSeconds(0.1f);
+        }
+        dashing = false;
+        dashInCD = true;
+        yield return new WaitForSeconds(dashCD);
+        dashInCD = false;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log(collision.collider.name);
     }
+
 
 }
