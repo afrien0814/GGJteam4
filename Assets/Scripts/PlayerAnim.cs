f285c3a5f2e81efd6c1d12cf263c21e277c5a364
catlop0202 <cvayiatetutu@gmail.com>
2021-01-30 15:29:33 +0800

    姆咪姆咪星爆爆

--- Assets/Scripts/PlayerAnim.cs
+++ Assets/Scripts/PlayerAnim.cs
@@ -7,7 +7,10 @@ public class PlayerAnim : MonoBehaviour
     Rigidbody2D get_velocity;
     Animator anim;
 
+    public Move skill_info;
     public Chaser chase_info;
+
+    Item.item_type former_item;
 
     private void Start()
     {
@@ -17,9 +20,41 @@ public class PlayerAnim : MonoBehaviour
 
     private void Update()
     {
+        //idle or walk
         anim.SetFloat("velocity", get_velocity.velocity.magnitude);
-        //anim.SetTrigger("skill",true);
+
+        //
+
+        //skill
+        if(former_item!=Item.item_type.nothing 
+            && skill_info.item_holding == Item.item_type.nothing)
+        {
+            anim.SetTrigger("skill");
+        }
+        former_item = skill_info.item_holding;
+
+        if(skill_info.jumping)
+        {
+            StartCoroutine(Jump());
+        }
+
+        //chasing
         anim.SetBool("find_target", chase_info.chasing);
         anim.SetBool("chased", chase_info.isChased);
+    }
+
+    IEnumerator Jump()
+    {
+        for(int i=0; i<10; i++)
+        {
+            this.transform.localScale += new Vector3(0.1f, 0.1f, 0);
+            yield return null;
+        }
+        for(int i=0; i<10; i++)
+        {
+            this.transform.localScale -= new Vector3(0.1f, 0.1f, 0);
+            yield return null;
+        }
+        yield break;
     }
 }
