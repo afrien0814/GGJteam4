c220302a9845ec73d5e9fc06255dc5ff7f173459
A_HAN <superman1230000@gmail.com>
2021-01-30 22:43:21 +0800

    map

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -12,17 +12,18 @@ public class MapGenerator : MonoBehaviour
     [Tooltip("每一個方格的大小(unity內部單位)")]
     public Vector2 unit_scale;
     public GameObject[] prefab_list;public string[] prefab_name;
-    public GameObject outer_map_obj;
     public List<Vector2> free_position_list = new List<Vector2>();
     public int width,length;
+
+    public GameObject[] item_list;
     void Awake(){
         if(mapGenerator == null){
             mapGenerator = this;
         }else{
             Destroy(this.gameObject);
         }   
-        
-        read_map();
+        // GameManager.gameManager.map_name;
+        read_map(/*GameManager.gameManager.map_name*/"map3.csv");
         generate_map();
     }
     void Start()
@@ -34,8 +35,8 @@ public class MapGenerator : MonoBehaviour
     {
         
     }
-    void read_map(){
-        StreamReader sr = new StreamReader(Application.streamingAssetsPath + "/map_test.csv");
+    void read_map(string map_name){
+        StreamReader sr = new StreamReader(Application.streamingAssetsPath + "/" + map_name);
         try {
             string line;bool first = true;
             while ((line = sr.ReadLine()) != null)
@@ -64,7 +65,7 @@ public class MapGenerator : MonoBehaviour
             for(int j=0;j < map[i].Length;j++){
                 for(int prefab_index=0;prefab_index<prefab_list.Length;prefab_index++){
                     if((map[i])[j] == prefab_name[prefab_index]){
-                        if((map[i])[j] != "255"){
+                        if((map[i])[j] != prefab_name[0]){//prefab_name[0] => blank
                             GameObject new_pref = Instantiate(prefab_list[prefab_index]);
                             new_pref.transform.position = new Vector3(0,0,0)+new Vector3(j*unit_scale.y,i*unit_scale.x,0);
                         }else{
@@ -88,21 +89,27 @@ public class MapGenerator : MonoBehaviour
         //     }
         // }
     }
-    // void get_free_coordinate_list(){
-    //     for (int x = 0; x < bounds.size.x; x++) {
-    //         for (int y = 0; y < bounds.size.y; y++) {
-    //             Vector3 pos = new Vector3((offset.x+lower_wall.x + x)*cellsize.x,(offset.y+lower_wall.y + y)*cellsize.y,-5f);
-    //             Ray myRay =  Camera.main.ScreenPointToRay(pos);
-    //             RaycastHit2D hit = Physics2D.Raycast(myRay.origin, myRay.direction, 10, 2);
-    //             if (hit.collider)
-    //             {
-    //                 print(pos.ToString()+"has object"+hit.collider.gameObject.activeSelf);
-    //                 GameObject gm = Instantiate(red);
-    //                 gm.transform.position = pos;
-    //             }
-    //         }
-    //     }
-    // }
+    public GameObject red , detector;
+    public  void get_free_coordinate_list(){
+        print(map[0].Length + " / " + map.Count);
+        for (int x = 0; x < map[0].Length; x++) {
+            for (int y = 0; y < map.Count; y++) {
+                //Ray myRay =  new Ray(new Vector3(unit_scale.x*x,unit_scale.y*y,-5),Vector3.back);
+                //Ray2D myRay = new Ray2D(new Vector2(unit_scale.x*x,unit_scale.y*y),new Vector2(100,100));
+                //Ray myRay = Camera.main.ScreenPointToRay(new Vector3(unit_scale.x*x,unit_scale.y*y,-2));
+                //print("ray  "+myRay.origin + " /dir " + myRay.direction);
+                //RaycastHit2D hit = Physics2D.Raycast(myRay.origin, myRay.direction, 10, -1);
+                //RaycastHit2D[] hit = Physics2D.RaycastAll(myRay.origin, myRay.direction);
+                //if (hit.Length != 0)
+                //{
+                    //print(new Vector3(unit_scale.x*x,unit_scale.y*y,0).ToString()+"has object"+hit[0].collider.gameObject.activeSelf);
+                    //GameObject gm = Instantiate(red);
+                    //gm.transform.position = new Vector3(unit_scale.x*x,unit_scale.y*y,0);
+                //}
+
+            }
+        }
+    }
 
 }
 
