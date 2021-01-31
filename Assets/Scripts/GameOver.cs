ca070071961ce8c3d7a468f2c81088ca5f6c8a3b
afrien0814 <49971202+afrien0814@users.noreply.github.com>
2021-01-31 13:54:43 +0800

    TBC

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class GameOver : MonoBehaviour
 {
-
+    
+    AudioSource audioSource;
     void Start()
     {
         GameManager.gameManager.callUI += OnGameEnd;
         transform.localScale = Vector2.zero;
+        audioSource = GetComponent<AudioSource>();
     }
 
     public void OnGameEnd(string callName)
@@ -16,7 +18,8 @@ public class GameOver : MonoBehaviour
         if (callName != this.name) return;
         //change icon here
         transform.localScale = Vector3.one;
-        
+        audioSource.time = 0.5f;
+        audioSource.Play();
     }
     public void OnRestartClick()
     {
